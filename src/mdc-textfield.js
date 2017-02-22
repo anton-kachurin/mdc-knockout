efe909fd8f763aa75a7420a4fed6df75f31a42c9
Anton Kachurin <kachurinanton@gmail.com>
2017-02-22 03:31:42 -0500

    feat: Support help text

--- src/mdc-textfield.js
+++ src/mdc-textfield.js
@@ -1,6 +1,10 @@
 var MDCTextfield = mdc.textfield.MDCTextfield;
 var foundation = mdc.textfield.MDCTextfieldFoundation;
 
+var randomStr = function (prefix) {
+  return prefix + '-' + Math.floor(Math.random() * 1000000)
+}
+
 ko.bindingHandlers['mdc-css'] = {
     init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
       console.log(bindingContext)
@@ -18,9 +22,14 @@ function TextfieldViewModel (params, instance) {
   var self = this;
   self.foundation = foundation;
   self.instance = instance;
+  self.ariaControls = randomStr('textfield-helptext');
 
   self.value = params.value;
   self.label = params.label;
+  self.help = params.help;
+  self.persistant = params.persistant;
+  var persistant = ko.unwrap(self.persistant);
+
 
   self.disabled = params.disabled;
   self.instance.disabled = ko.unwrap(self.disabled);
@@ -37,12 +46,33 @@ TextfieldViewModel.prototype.fn = function () {
 
 var template = `
 <label class="mdc-textfield" data-bind="mdc-css: { UPGRADED: value }">
-  <input type="text" class="mdc-textfield__input" data-bind="value: value">
+  <input type="text" class="mdc-textfield__input" data-bind="
+    value: value,
+    attr: { 'aria-controls': ko.unwrap(help) && ariaControls}
+    ">
   <span class="mdc-textfield__label" data-bind="
     text: label,
     mdc-css: { LABEL_FLOAT_ABOVE: value }"
   ></span>
 </label>
+<!-- ko if: help -->
+  <!-- ko ifnot: persistant -->
+    <p class="mdc-textfield-helptext"
+       data-bind="
+        text: help,
+        attr: { id: ariaControls }
+       ">
+    </p>
+  <!-- /ko -->
+  <!-- ko if: persistant -->
+    <p class="mdc-textfield-helptext mdc-textfield-helptext--persistent"
+       data-bind="
+        text: help,
+        attr: { id: ariaControls }
+       ">
+    </p>
+  <!-- /ko -->
+<!-- /ko -->
 `;
 
 ko.components.register('mdc-textfield', {
