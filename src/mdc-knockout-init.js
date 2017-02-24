893780016391bd82e62370b10c293adbaeffa253
Anton Kachurin <kachurinanton@gmail.com>
2017-02-23 19:53:06 -0500

    chore: Rename auto-init to knockout-init
    This new file will contain all extensions to knockout, such as
    new bindings and component registration

---
+++ src/mdc-knockout-init.js
@@ -0,0 +1,85 @@
+if (!ko.getBindingHandler('mdc-instance')) {
+  ko.bindingHandlers['mdc-instance'] = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+      var root = bindingContext.$component.root;
+      var instance = bindingContext.$component.attachTo(root);
+      valueAccessor()(instance);
+      bindingContext.$component.initialize()
+    }
+  };
+}
+
+if (!ko.getBindingHandler('mdc-bindings')) {
+  ko.bindingHandlers['mdc-bindings'] = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+      ko.applyBindingsToNode(element, valueAccessor(), bindingContext);
+    }
+  };
+}
+
+if (!ko.getBindingHandler('mdc-css')) {
+  ko.bindingHandlers['mdc-css'] = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+      function toggleClass(className, value) {
+        ko.utils.toggleDomNodeCssClass(element, className, value);
+      }
+
+      var classList = ko.unwrap(valueAccessor());
+
+      ko.utils.objectForEach(classList, function (key, value) {
+        var className = bindingContext.$component.foundation.cssClasses[key];
+        if (ko.unwrap(value)) {
+          toggleClass(className, true);
+        }
+        if (ko.isSubscribable(value)) {
+          value.subscribe( function (value) {
+            toggleClass(className, value)
+          });
+        }
+      });
+    }
+  };
+}
+
+if (!ko.getBindingHandler('mdc-attr')) {
+  ko.bindingHandlers['mdc-attr'] = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+      var attrList = ko.unwrap(valueAccessor());
+
+      ko.utils.objectForEach(attrList, function (key, value) {
+        var attrName = bindingContext.$component.foundation.strings[key];
+        if (ko.unwrap(value)) {
+          element.setAttribute(attrName, true);
+        }
+      });
+    }
+  };
+}
+
+window.register = window.register || function (name, viewModelConstructor, template) {
+  ko.components.register(name, {
+      viewModel: {
+        createViewModel: function(params, componentInfo) {
+          var element = componentInfo.element;
+          var root = element.children[0];
+          var attributes = element.attributes;
+          var attrs = {}
+          var names = [];
+          ko.utils.arrayForEach(attributes, function (attr) {
+            var attrName = attr.name.toLowerCase();
+            if (attrName != 'params'
+             && attrName != 'class'
+             && attrName != 'data-bind') {
+              attrs[attr.name] = attr.value;
+              names.push(attr.name);
+            }
+          });
+          ko.utils.arrayForEach(names, function (name) {
+            element.removeAttribute(name);
+          });
+          return new viewModelConstructor(root, params, attrs);
+        }
+      },
+      template: template
+  });
+};
