25a2836ad6ead985d7436c8b9c02fd9209cd608d
Anton Kachurin <kachurinanton@gmail.com>
2017-02-24 03:03:56 -0500

    feat: Add mdc-form-field component

---
+++ src/mdc-knockout-form-field.js
@@ -0,0 +1,27 @@
+var MDCFormField = mdc.formField.MDCFormField;
+var foundation = mdc.formField.MDCFormFieldFoundation;
+
+FormFieldViewModel.prototype = Object.create(ComponentViewModel.prototype);
+FormFieldViewModel.prototype.constructor = FormFieldViewModel;
+
+function FormFieldViewModel (root, params, attrs) {
+  ComponentViewModel.call(this, root, params, attrs, foundation, MDCFormField);
+}
+
+FormFieldViewModel.prototype.defaultParams = function () {
+  return {
+    alignEnd: false
+  }
+};
+
+var template = `
+<div class="mdc-form-field" data-bind="
+  css: { 'mdc-form-field--align-end': alignEnd }
+">
+  <!-- ko template: { nodes: $componentTemplateNodes, data: $parent } --><!-- /ko -->
+  <label data-bind="mdc-bindings: bindings, attr: attrs">Input Label</label>
+</div>
+<span data-bind="mdc-instance: $component.instance"></span>
+`;
+
+register('mdc-form-field', FormFieldViewModel, template);
