1f5db3e2196c1a121b1524cc4bf27dd4e5859809
Anton Kachurin <kachurinanton@gmail.com>
2017-02-25 03:49:33 -0500

    feat: Change mdc-child call signature

--- src/mdc-knockout-form-field.js
+++ src/mdc-knockout-form-field.js
@@ -18,7 +18,7 @@ var template = `
 <div class="mdc-form-field" data-bind="
   css: { 'mdc-form-field--align-end': alignEnd }
 ">
-  <!-- ko mdc-child: $componentTemplateNodes --><!-- /ko -->
+  <!-- ko mdc-child: true --><!-- /ko -->
   <label data-bind="mdc-bindings: bindings, attr: attrs"></label>
 </div>
 <!-- ko mdc-instance: true --><!-- /ko -->
