cc91969640f54f4ab16e4ab66beac280088f81d6
Anton Kachurin <kachurinanton@gmail.com>
2017-02-27 15:23:25 -0500

    feat: Add mdc-knockout-radio component

---
+++ src/mdc-knockout-radio.js
@@ -0,0 +1,79 @@
+var MDCCheckbox = mdc.radio.MDCRadio;
+var foundation = mdc.radio.MDCRadioFoundation;
+
+(function(MDCCheckbox, foundation){
+
+RadioViewModel.prototype = Object.create(ComponentViewModel.prototype);
+RadioViewModel.prototype.constructor = RadioViewModel;
+
+function RadioViewModel (root, params, attrs) {
+  ComponentViewModel.call(this, root, params, attrs, foundation, MDCCheckbox);
+}
+
+RadioViewModel.prototype.defaultParams = function () {
+  return {
+    isChecked: false,
+    disable: false
+  }
+}
+
+RadioViewModel.prototype.initialize = function (parent) {
+  var self = this;
+  function setChecked () {
+    // check radio input knockout-way
+    if (self.bindings.hasOwnProperty('checked')
+    && ko.isObservable(self.bindings.checked)) {
+      self.bindings.checked(self.attrs.value);
+    }
+  }
+
+  var isChecked = self.isChecked;
+
+  if (ko.unwrap(isChecked)) {
+    setChecked();
+  }
+
+  if (ko.isSubscribable(isChecked)) {
+    isChecked.subscribe(function (value) {
+      if (value) {
+        setChecked();
+      }
+      else if (self.attrs.value == ko.unwrap(self.bindings.checked)) {
+        self.bindings.checked(undefined);
+      }
+    });
+
+    if (self.bindings.hasOwnProperty('checked')
+    && ko.isSubscribable(self.bindings.checked)) {
+      self.bindings.checked.subscribe(function (value) {
+        if (value == self.attrs.value) {
+          isChecked(true);
+        }
+        else {
+          isChecked(false);
+        }
+      });
+    }
+  }
+
+  if (parent) {
+    parent.instance().input = self.instance();
+  }
+}
+
+template = `
+<div class="mdc-radio" data-bind="mdc-css: { DISABLED: disable }">
+  <input class="mdc-radio__native-control"
+         type="radio"
+         data-bind="attr: attrs, mdc-bindings: bindings, disable: disable">
+  <div class="mdc-radio__background">
+    <div class="mdc-radio__outer-circle"></div>
+    <div class="mdc-radio__inner-circle"></div>
+  </div>
+</div>
+<!-- ko mdc-instance: true --><!-- /ko -->
+`;
+
+register('mdc-radio', RadioViewModel, template);
+
+}(MDCCheckbox, foundation))
