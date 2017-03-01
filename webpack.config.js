b2eb7c7c2e3bb57017d5ebfe3e4d0c2865002317
Anton Kachurin <kachurinanton@gmail.com>
2017-02-28 23:15:22 -0500

    chore: Change building options

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,27 @@ var path = require('path');
 const PATH = path.resolve(__dirname, 'dist');
 const PUBLIC_PATH = '/dist/';
 
-module.exports = {
-  entry: './src/material-components-knockout.js',
-  output: {
-    filename: 'mdc-knockout.js',
-    path: PATH,
-    publicPath: PUBLIC_PATH
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015']
+module.exports = env => {
+  const IS_PROD = env === 'prod';
+
+  return {
+    entry: './src/material-components-knockout.js',
+    output: {
+      filename: 'mdc-knockout' + (IS_PROD ? '.min' : '') + '.js',
+      path: PATH,
+      publicPath: PUBLIC_PATH
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
         }
-      }
-    ]
-  },
-  devtool: 'source-map'
+      ]
+    },
+    devtool: IS_PROD ? false : 'source-map'
+  }
 };
