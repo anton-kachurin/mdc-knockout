5349c45565082b77c73dbb13e7e496f36d95f3f0
Anton Kachurin <kachurinanton@gmail.com>
2017-03-06 23:32:20 -0500

    fix: Use --disabled class for disabled switch

--- src/mdc-knockout-switch.js
+++ src/mdc-knockout-switch.js
@@ -9,7 +9,8 @@ export default class SwitchViewModel extends PlainViewModel {
 
   defaultParams () {
     return {
-      label: ''
+      label: '',
+      disable: false
     }
   }
 
@@ -19,10 +20,15 @@ export default class SwitchViewModel extends PlainViewModel {
 }
 
 const template =
-`<div class="mdc-switch">
+`<div class="mdc-switch" data-bind="
+  css: {
+    'mdc-switch--disabled': disable
+  }
+">
   <input type="checkbox" class="mdc-switch__native-control" data-bind="
     mdc-bindings: bindings,
-    attr: attrs
+    attr: attrs,
+    disable: disable
   " />
   <div class="mdc-switch__background">
     <div class="mdc-switch__knob"></div>
