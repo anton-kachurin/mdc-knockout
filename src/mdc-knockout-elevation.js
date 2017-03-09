e356c9efb4bb9248777c840710e8cda6d46ec1cc
Anton Kachurin <kachurinanton@gmail.com>
2017-03-09 10:37:11 -0500

    refactor: Rewrite elevation to use mdc-parent binding

--- src/mdc-knockout-elevation.js
+++ src/mdc-knockout-elevation.js
@@ -5,30 +5,32 @@ const Z_PREFIX = 'mdc-elevation--z';
 
 export default class ElevationViewModel extends PlainViewModel {
   extend () {
+    const css = {};
     if (ko.isSubscribable(this.z)) {
-      this.css['mdc-elevation-transition'] = true;
+      css['mdc-elevation-transition'] = true;
       for (let i=0; i<=24; i++) {
-        this.css[Z_PREFIX + i] = ko.observable(false);
+        css[Z_PREFIX + i] = ko.observable(false);
       }
 
       let prev = ko.unwrap(this.z);
       this.z.subscribe((value) => {
-        this.css[Z_PREFIX + prev](false);
-        this.css[Z_PREFIX + value](true);
+        css[Z_PREFIX + prev](false);
+        css[Z_PREFIX + value](true);
         prev = value;
       });
 
-      this.css[Z_PREFIX + prev](true);
+      css[Z_PREFIX + prev](true);
     }
     else{
-      this.css[Z_PREFIX + this.z] = true;
+      css[Z_PREFIX + this.z] = true;
     }
+
+    this.bindings.css = Object.assign({}, this.bindings.css, css);
   }
 
   defaultParams () {
     return {
-      z: 0,
-      css: {}
+      z: 0
     }
   }
 
