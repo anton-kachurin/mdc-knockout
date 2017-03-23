00fa2a7d303e93e460a83aa931f6e86894028686
Anton Kachurin <kachurinanton@gmail.com>
2017-03-23 19:22:55 -0400

    feat(switch): Allow standard attributes

--- src/mdc-knockout-switch.js
+++ src/mdc-knockout-switch.js
@@ -3,15 +3,31 @@ import template from './templates/switch.html';
 
 export default class SwitchViewModel extends PlainViewModel {
   extend () {
+    // make sure that --disabled class is set if necessary
+    if (!('disable' in this.bindings)) {
+      if ('disabled' in this.attrs) {
+        this.bindings.disable = true;
+      }
+    }
+
     if (!this.attrs['id']) {
       this.attrs['id'] = this.randomPrefixed('switch-auto-id');
+    }
+
+    this.nodeFilter = (child) => {
+      if (!this.label && child.nodeType == 3) {
+        let text = child.textContent;
+        if (text.match(/[^\s]/)) {
+          this.label = text;
+        }
+      }
+      return false;
     }
   }
 
   defaultParams () {
     return {
-      label: '',
-      disable: false
+      label: ''
     }
   }
 
