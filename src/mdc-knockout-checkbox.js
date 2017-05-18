123f30ce85179bcadd5a9fba26227bd99b33a334
Anton Kachurin <kachurinanton@gmail.com>
2017-05-18 00:56:26 -0400

    refactor(checkbox): Use HookableComponentViewModel

--- src/mdc-knockout-checkbox.js
+++ src/mdc-knockout-checkbox.js
@@ -1,28 +1,52 @@
-import {CheckableComponentViewModel} from './mdc-knockout-base';
+import {HookableComponentViewModel} from './mdc-knockout-base';
 import CheckboxTemplate from './templates/checkbox.html';
 
-class CheckboxViewModel extends CheckableComponentViewModel {
-  initialize () {
-    const checked = this.bindings.checked;
+class CheckboxViewModel extends HookableComponentViewModel {
+  extend () {
+    if (!this.attrs['id']) {
+      this.attrs['id'] = this.randomPrefixed('checkable-auto-id');
+    }
+  }
+
+  initialize (parent) {
+    if (parent && ko.isSubscribable(parent.for)) {
+      parent.instance.input = this.instance;
+      parent.for(this.attrs['id']);
+    }
+
     this.instance.indeterminate = ko.unwrap(this.indeterminate);
     if (ko.isSubscribable(this.indeterminate)) {
-      this.track = this.indeterminate.subscribe(
-        value => { if (value) this.instance.indeterminate = true }
-      );
-      if (ko.isSubscribable(checked)) {
-        this.track = checked.subscribe(value => {
-          this.indeterminate(false);
-          if (this.instance.indeterminate) {
-             this.instance.indeterminate = false;
-          }
-        });
-      }
+      this.track = this.indeterminate.subscribe(value => {
+        this.instance.indeterminate = value;
+      })
     }
   }
 
   defaultParams () {
     return {
       indeterminate: false
+    }
+  }
+
+  get forceBindings () {
+    return {
+      checked: () => ('checked' in this.attrs),
+      disable: () => ('disabled' in this.attrs),
+    }
+  }
+
+  get hookedElement () {
+    return this.root.querySelector('input');
+  }
+
+  get hookedProperties () {
+    return {
+      checked: state => this.hookedElement.indeterminate = false,
+      indeterminate: state => {
+        if (ko.isSubscribable(this.indeterminate)) {
+          this.indeterminate(state);
+        }
+      }
     }
   }
 }
