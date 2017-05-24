77bb5a4dc6444b6d5557768b9153ae11f56ef51f
Anton Kachurin <kachurinanton@gmail.com>
2017-05-24 18:58:31 -0400

    test(switch): Add tests to check that forceBindings work

--- test/unit/switch.component.test.js
+++ test/unit/switch.component.test.js
@@ -1,5 +1,6 @@
 import {assert} from 'chai';
 import bel from 'bel';
+import domEvents from 'dom-events';
 import augment from '../../src/mdc-knockout-augment';
 import {SwitchTemplate, SwitchViewModel} from '../../src/mdc-knockout-switch';
 
@@ -9,7 +10,31 @@ augment.registerComponent(ko, 'mdc-switch', SwitchTemplate(), SwitchViewModel);
 
 suite('switch component');
 
-test('checked plain param controls the state of the switch', (done) => {
+test('switch is on when "checked" attribute present', (done) => {
+  const component = bel`<mdc-switch checked></mdc-switch>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isOk(input.checked);
+
+    done();
+  });
+});
+
+test('switch is off when no "checked" attribute present', (done) => {
+  const component = bel`<mdc-switch></mdc-switch>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isNotOk(input.checked);
+
+    done();
+  });
+});
+
+test('"checked" plain param controls the state of the switch', (done) => {
   const component = bel`<mdc-switch params="checked: true"></mdc-switch>`;
   ko.applyBindings({}, component);
 
@@ -21,7 +46,7 @@ test('checked plain param controls the state of the switch', (done) => {
   });
 });
 
-test('checked observable param controls the state of the switch', (done) => {
+test('"checked" observable param controls the state of the switch', (done) => {
   const component = bel`<mdc-switch params="checked: isOn"></mdc-switch>`;
   const isOn = ko.observable(false);
   ko.applyBindings({isOn: isOn}, component);
@@ -37,7 +62,31 @@ test('checked observable param controls the state of the switch', (done) => {
   });
 });
 
-test('disable plain param controls disabled state', (done) => {
+test('switch is disabled when "disabled" attribute present', (done) => {
+  const component = bel`<mdc-switch disabled></mdc-switch>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isOk(input.disabled);
+
+    done();
+  });
+});
+
+test('switch is enabled when no "disabled" attribute present', (done) => {
+  const component = bel`<mdc-switch></mdc-switch>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isNotOk(input.disabled);
+
+    done();
+  });
+});
+
+test('"disable" plain param controls disabled state', (done) => {
   const component = bel`<mdc-switch params="disable: true"></mdc-switch>`;
   ko.applyBindings({}, component);
 
@@ -49,7 +98,7 @@ test('disable plain param controls disabled state', (done) => {
   });
 });
 
-test('disable observable param controls disabled state', (done) => {
+test('"disable" observable param controls disabled state', (done) => {
   const component = bel`<mdc-switch params="disable: isDisabled"></mdc-switch>`;
   const isDisabled = ko.observable(false);
   ko.applyBindings({isDisabled: isDisabled}, component);
@@ -65,7 +114,7 @@ test('disable observable param controls disabled state', (done) => {
   });
 });
 
-test('label plain param sets the label text', (done) => {
+test('"label" plain param sets the label text', (done) => {
   const component = bel`<mdc-switch params="label: 'switch label'"></mdc-switch>`;
   ko.applyBindings({}, component);
 
@@ -77,7 +126,7 @@ test('label plain param sets the label text', (done) => {
   });
 });
 
-test('label observable param sets the label text', (done) => {
+test('"label" observable param sets the label text', (done) => {
   const component = bel`<mdc-switch params="label: labelText"></mdc-switch>`;
   const labelText = ko.observable('a');
   ko.applyBindings({labelText: labelText}, component);
@@ -124,6 +173,51 @@ test('label as param has higher priority than label as inner html', (done) => {
   setTimeout(() => {
     const label = component.querySelector('label');
     assert.equal(label.textContent, 'as param');
+
+    done();
+  });
+});
+
+test('can use an observable array with "checked" binding', (done) => {
+  const component = bel`
+    <div>
+      <mdc-switch id="c1" value="val1" params="checked: checkedItems"></mdc-switch>
+      <mdc-switch id="c2" value="val2" params="checked: checkedItems"></mdc-switch>
+    </div>
+  `;
+  const checkedItems = ko.observableArray();
+  ko.applyBindings({checkedItems: checkedItems}, component);
+
+  setTimeout(() => {
+    const c1 = component.querySelector('#c1');
+    const c2 = component.querySelector('#c2');
+    assert.deepEqual(checkedItems(), []);
+
+    domEvents.emit(c1, 'click');
+    assert.deepEqual(checkedItems(), ['val1']);
+
+    domEvents.emit(c2, 'click');
+    assert.deepEqual(checkedItems(), ['val1', 'val2']);
+
+    domEvents.emit(c1, 'click');
+    assert.deepEqual(checkedItems(), ['val2']);
+
+    done();
+  });
+});
+
+test('can use checkedValue binding', (done) => {
+  const component = bel`<mdc-switch params="checked: isChecked, checkedValue: val"></mdc-switch>`;
+  const isChecked = ko.observableArray();
+  const val = {some: 'value'};
+  ko.applyBindings({isChecked: isChecked, val: val}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.deepEqual(isChecked(), []);
+
+    domEvents.emit(input, 'click');
+    assert.deepEqual(isChecked(), [{some: 'value'}]);
 
     done();
   });
