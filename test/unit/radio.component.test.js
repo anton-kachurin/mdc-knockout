b03e29664b6713a0330e4a5a2afaef56b079a8ca
Anton Kachurin <kachurinanton@gmail.com>
2017-05-24 20:33:57 -0400

    test(radio): Add component tests

---
+++ test/unit/radio.component.test.js
@@ -0,0 +1,150 @@
+import {assert} from 'chai';
+import bel from 'bel';
+import domEvents from 'dom-events';
+import augment from '../../src/mdc-knockout-augment';
+import {RadioTemplate, RadioViewModel} from '../../src/mdc-knockout-radio';
+import {MDCRadio, MDCRadioFoundation} from '@material/radio';
+
+import ko from 'knockout';
+augment.registerBindings(ko);
+augment.registerComponent(ko, 'mdc-radio', RadioTemplate(), RadioViewModel, MDCRadio, MDCRadioFoundation);
+
+suite('radio component');
+
+test('radio is checked according to "checked" attribute presence', (done) => {
+  const component = bel`
+  <div>
+    <mdc-radio value="val1" checked></mdc-radio>
+    <mdc-radio value="val2"></mdc-radio>
+  </div>
+  `;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input1 = component.querySelector('input[value=val1]');
+    const input2 = component.querySelector('input[value=val2]');
+    assert.isOk(input1.checked);
+    assert.isNotOk(input2.checked);
+
+    done();
+  });
+});
+
+test('"checked" observable param controls the state of the radio', (done) => {
+  const component = bel`
+  <div>
+    <mdc-radio value="val1" params="checked: radio"></mdc-radio>
+    <mdc-radio value="val2" params="checked: radio"></mdc-radio>
+  </div>
+  `;
+  const radio = ko.observable();
+  ko.applyBindings({radio: radio}, component);
+
+  setTimeout(() => {
+    const input1 = component.querySelector('input[value=val1]');
+    const input2 = component.querySelector('input[value=val2]');
+    assert.isNotOk(input1.checked);
+    assert.isNotOk(input2.checked);
+
+    radio('val1');
+    assert.isOk(input1.checked);
+    assert.isNotOk(input2.checked);
+
+    radio('val2');
+    assert.isNotOk(input1.checked);
+    assert.isOk(input2.checked);
+
+    done();
+  });
+});
+
+test('radio is disabled when "disabled" attribute present', (done) => {
+  const component = bel`<mdc-radio disabled></mdc-radio>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isOk(input.disabled);
+
+    done();
+  });
+});
+
+test('radio is enabled when no "disabled" attribute present', (done) => {
+  const component = bel`<mdc-radio></mdc-radio>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isNotOk(input.disabled);
+
+    done();
+  });
+});
+
+test('"disable" plain param controls disabled state', (done) => {
+  const component = bel`<mdc-radio params="disable: true"></mdc-radio>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isOk(input.disabled);
+
+    done();
+  });
+});
+
+test('"disable" observable param controls disabled state', (done) => {
+  const component = bel`<mdc-radio params="disable: isDisabled"></mdc-radio>`;
+  const isDisabled = ko.observable(false);
+  ko.applyBindings({isDisabled: isDisabled}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isNotOk(input.disabled);
+
+    isDisabled(true);
+    assert.isOk(input.disabled);
+
+    done();
+  });
+});
+
+test('can use checkedValue binding', (done) => {
+  const component = bel`
+  <div>
+    <mdc-radio id="r1" params="checked: radio, checkedValue: val1"></mdc-radio>
+    <mdc-radio id="r2" params="checked: radio, checkedValue: val2"></mdc-radio>
+  </div>
+  `;
+  const radio = ko.observable();
+  const val1 = {some: 'value1'};
+  const val2 = {some: 'value2'};
+  ko.applyBindings({radio: radio, val1: val1, val2: val2}, component);
+
+  setTimeout(() => {
+    const input1 = component.querySelector('#r1');
+    const input2 = component.querySelector('#r2');
+    assert.isUndefined(radio());
+
+    domEvents.emit(input1, 'click');
+    assert.deepEqual(radio(), {some: 'value1'});
+
+    domEvents.emit(input2, 'click');
+    assert.deepEqual(radio(), {some: 'value2'});
+
+    done();
+  });
+});
+
+test('if no id was set to the component, it is generated automatically', (done) => {
+  const component = bel`<mdc-radio></mdc-radio>`;
+  ko.applyBindings({}, component);
+
+  setTimeout(() => {
+    const input = component.querySelector('input');
+    assert.isOk(input.id.length);
+
+    done();
+  });
+});
