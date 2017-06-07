f98c4f688d14f2a9ee905a62c6e9cfaef8189504
Anton Kachurin <kachurinanton@gmail.com>
2017-06-06 23:23:50 -0400

    refactor(textfield): Get rid of Knockout dependencies

--- src/mdc-knockout-textfield.js
+++ src/mdc-knockout-textfield.js
@@ -7,74 +7,48 @@ class TextfieldViewModel extends ComponentViewModel {
       this.attrs['id'] = this.randomPrefixed('textfield-auto-id');
     }
 
-    this.ariaControls = this.randomPrefixed('textfield-helptext');
-    this.attrs['aria-controls'] = ko.unwrap(this.help) && this.ariaControls;
+    this.attrs['aria-controls'] = this.randomPrefixed('textfield-helptext');
 
-    this.updateLabel(this.label);
+    if (this.fullwidth) {
+      this.attrs['aria-label'] = this.label;
+      this.attrs['placeholder'] = this.label;
+    }
 
     this.nodeFilter = (child) => {
-      function textNotEmpty (text) {
-        return text.match(/[^\s]/);
-      }
-
       if (!this.label && child.nodeType == 3) {
         let text = child.textContent;
-        if (textNotEmpty(text)) {
-          this.updateLabel(text);
-        }
-      }
-      if (!this.help && child.nodeType == 1 && child.tagName == 'P') {
-        let text = child.textContent;
-        if (textNotEmpty(text)) {
-          this.help = text;
-          for (let attr of child.attributes) {
-            if (attr.name == 'persistent') {
-              this.persistent = true;
-            }
 
-            if (attr.name == 'validation') {
-              this.validation = true;
-            }
+        // ignore empty text nodes
+        if (text.match(/[^\s]/)) {
+          this.label = text;
+          if (this.fullwidth) {
+            this.attrs['aria-label'] = text;
+            this.attrs['placeholder'] = text;
           }
         }
       }
+
+      if (!this.help && child.nodeType == 1 && child.tagName == 'P') {
+        let text = child.textContent;
+        this.help = text;
+        for (let attr of child.attributes) {
+          if (attr.name == 'persistent') {
+            this.persistent = true;
+          }
+
+          if (attr.name == 'validation') {
+            this.validation = true;
+          }
+        }
+      }
+
       return false;
     }
   }
 
-  updateLabel (label) {
-    label = ko.unwrap(label);
-    label = String(label).trim();
-    if (this.fullwidth) {
-      if (!this.attrs['aria-label']) {
-        this.attrs['aria-label'] = ko.observable();
-      }
-      this.attrs['aria-label'](label);
-
-      if (!this.attrs['placeholder']) {
-        this.attrs['placeholder'] = ko.observable();
-      }
-      this.attrs['placeholder'](label);
-    }
-
-    if (ko.isWritableObservable(this.label)) {
-      this.label(label);
-    }
-    else {
-      this.label = label;
-    }
-  }
-
-  initialize () {
-    if ('disabled' in this.attrs) {
-      this.disable = true;
-    }
-
-    this.instance.disabled = ko.unwrap(this.disable);
-    if (ko.isSubscribable(this.disable)) {
-      this.track = this.disable.subscribe( value => {
-        this.instance.disabled = value;
-      });
+  get forceBindings () {
+    return {
+      disable: () => ('disabled' in this.attrs)
     }
   }
 
@@ -86,8 +60,7 @@ class TextfieldViewModel extends ComponentViewModel {
       validation: false,
       invalid: false,
       multiline: false,
-      fullwidth: false,
-      disable: false
+      fullwidth: false
     }
   }
 
