842e4a0de5e7f0f0fdd2e88706fbe85122972024
Anton Kachurin <kachurinanton@gmail.com>
2017-06-17 23:19:42 -0400

    feat(fab): Add "icon" parameter

--- src/mdc-knockout-fab.js
+++ src/mdc-knockout-fab.js
@@ -3,10 +3,23 @@ import FABTemplate from './templates/fab.html';
 
 class FABViewModel extends PlainViewModel {
 
+  nodeFilter (child) {
+    if (!this.icon && child.nodeType == 3) {
+      let text = child.textContent;
+
+      // ignore empty text nodes
+      if (text.match(/[^\s]/)) {
+        this.icon = text;
+      }
+    }
+    return false;
+  }
+
   get defaultParams () {
     return {
       mini: false,
-      plain: false
+      plain: false,
+      icon: ''
     }
   }
 }
--- test/unit/fab.class.test.js
+++ test/unit/fab.class.test.js
@@ -25,3 +25,52 @@ test('"plain" property exists', () => {
 test('"plain" has a default value of false', () => {
   assert.equal(vm.plain, false);
 });
+
+test('"icon" property exists', () => {
+  assert.property(vm, 'icon');
+});
+
+test('"icon" has a default value of ""', () => {
+  assert.equal(vm.icon, "");
+});
+
+test('nodeFilter method exists', () => {
+  assert.property(vm, 'nodeFilter');
+});
+
+test('nodeFilter method return value is false', () => {
+  assert.isNotOk(vm.nodeFilter({}));
+});
+
+test('nodeFilter updates "icon" property if it is empty', () => {
+  const vm = setupTestVM();
+  assert.equal(vm.icon, '');
+
+  vm.nodeFilter({
+    nodeType: 3,
+    textContent: 'icon'
+  });
+  assert.equal(vm.icon, 'icon');
+});
+
+test('nodeFilter does not update "icon" property if its parameter is not a text node', () => {
+  const vm = setupTestVM();
+  assert.equal(vm.icon, '');
+
+  vm.nodeFilter({
+    nodeType: 1,
+    textContent: 'icon'
+  });
+  assert.equal(vm.icon, '');
+});
+
+test('nodeFilter does not update "icon" property if child node has only whitespaces', () => {
+  const vm = setupTestVM();
+  assert.equal(vm.icon, '');
+
+  vm.nodeFilter({
+    nodeType: 3,
+    textContent: ' '
+  });
+  assert.equal(vm.icon, '');
+});
